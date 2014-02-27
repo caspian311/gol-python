ddf526bffd5957f88f5778c1b15bf0bfde4d918c
Matt Todd <matt.todd@asynchrony.com>
2014-02-27 07:40:00 -0600

    main is simpler

--- src/main.py
+++ src/main.py
@@ -4,24 +4,17 @@ import time
 from game_of_life import GameOfLife
 from display import Display
 
-class Main:
-   def __init__(self, size):
-      self._gol = GameOfLife(size)
-      self._display = Display()
+def run_game(size):
+   gol = GameOfLife(size)
+   display = Display()
 
-   def _do_cycle(self):
-      self._display.show_board(self._gol.get_board())
-
-   def start(self):
-      while True:
-         self._do_cycle()
-         time.sleep(1)
-
+   while True:
+      display.show_board(gol.get_board())
+      time.sleep(1)
 
 if __name__ == "__main__":
-   print 'recieved ' + str(sys.argv)
    if len(sys.argv) == 2:
       size = int(sys.argv[1])
    else:
       size = 10
-   Main(size).start()
+   run_game(size)
